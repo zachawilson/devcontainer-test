9b597cca6a254a0cab096b469866a3a5f329897c
zachawilson <zachawilson@users.noreply.github.com>
2020-06-19 18:27:43 -0700

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -25,13 +25,13 @@ if [ 0 -ne $? ]; then
 fi
 
 PACKER_VERSION=1.4.5
-echo Download packer version $${PACKER_VERSION}
-wget https://releases.hashicorp.com/packer/$${PACKER_VERSION}/packer_$${PACKER_VERSION}_linux_amd64.zip
-unzip packer_$${PACKER_VERSION}_linux_amd64.zip
+echo Download packer version ${PACKER_VERSION}
+wget https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
+unzip packer_${PACKER_VERSION}_linux_amd64.zip
 sudo mv packer /usr/local/bin/
 
 TERRAFORM_VERSION=0.12.20
-echo Download terraform version $${TERRAFORM_VERSION}
-wget https://releases.hashicorp.com/terraform/$${TERRAFORM_VERSION}/terraform_$${TERRAFORM_VERSION}_linux_amd64.zip
-unzip terraform_$${TERRAFORM_VERSION}_linux_amd64.zip
+echo Download terraform version ${TERRAFORM_VERSION}
+wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
